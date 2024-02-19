79cfe1b930b6030a04e7bf9295948f8d91c5161f
manhprofestion <nguyendomanhtb@gmail.com>
2024-02-19 12:55:09 +0700

    add test

--- btvn/src/bai2/bai2_3.java
+++ btvn/src/bai2/bai2_3.java
@@ -1,5 +1,37 @@
 package bai2;
+import java.util.*;
+
+//Nhập vào 1 số, kiểm tra: > 0 in ra Positive; < 0 in ra Negative; = 0 in ra 0
+//phân tích: 
 
 public class bai2_3 {
+	public static void main(String [] args) {
 
+		Scanner input = new Scanner (System.in);
+		while(true) {
+		int number = InputNumber(input);
+		CheckNumber(number);
+		}	
+	}
+	
+	public static int InputNumber(Scanner input) {
+		int n;
+		do{
+		System.out.print("Nhap vao so can kiem tra: ");
+			n = input.nextInt();
+			return n;
+		}while(true);
+	}
+	public static void CheckNumber (int n) {
+		if(n>0) {
+			System.out.println("Positive");
+		}
+		else if(n<0) {
+			System.out.println("Negative");
+		}
+		else {
+			System.out.println("0");
+		}
+		
+	}
 }
---
+++ btvn/src/bai2/bai2_4.java
@@ -0,0 +1,27 @@
+package bai2;
+import java.util.*;
+
+// Tính giai thừa
+// In ra 20 số đầu tiên của dãy fibo
+// Tính lãi kép cho một ngân hàng. Đầu vào có số tiền gửi, lãi suất và số tháng gửi
+// In ra các số từ 1 --> 100. Nếu số đó chia hết cho 3 in ra từ "Hello", chia hết cho 5 in ra từ "World",
+//						chia hết cho cả 3 và 5 in ra từ "HelloWorld". Các số khác in ra số như bình thường
+
+//
+public class bai2_4 {
+	public static void main() {
+		Scanner input = new Scanner (System.in);
+		InputNumber(input);
+	}
+	
+	public static void InputNumber(Scanner input) {
+		int n;
+		System.out.print("Nhap vao so  ");
+		n = input.nextInt();
+	}
+	
+	public static void GiaiThua(int n) {
+		
+	}
+	
+}
